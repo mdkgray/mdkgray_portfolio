f8e3add3e5d9562e977a749893529a4ee3bdc4ed
Mackenzie Gray <mdkulen@gmail.com>
2022-09-09 11:32:15 +1000

    added style to homepage with js clock

---
+++ assets/js/script.js
@@ -0,0 +1,69 @@
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            const target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 10
+                }, 400);
+                return false;
+            }
+        }
+    });
+});
+
+function round(value, precision) {
+    const multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
+
+$(document).ready(function(){
+    displayTime();
+    setInterval(displayTime, 1000);
+
+    let zip, country, locIP, latIP, lonIP = '';
+
+    /* https://ipinfo.io/json?token=62f16ef67528e4 */
+
+    $.get("https://ipinfo.io/json", function (response) {
+        $("#ip").html("IP: " + response.ip);
+        locIP = (response.loc).split(",");
+        latIP = locIP[0];
+        lonIP = locIP[1];
+        if(response.city != ''){
+            $("#address").html(response.city + ", " + response.region);
+            $("#details").html(JSON.stringify(response, null, 4));
+        }
+        console.log('city ' + response.city);
+    }, "jsonp");
+
+    setTimeout(function(){
+        const tempString = "https://api.openweathermap.org/data/2.5/weather?lat=" + latIP + "&lon=" + lonIP + "&appid=62586c3c7465e498380af78d50211e67&units=metric&lang=it";
+        $.get(tempString, function (response) {
+        $('.temperatura').text((Math.round(response.main.temp * 10) / 10).toFixed(1));
+        }, "jsonp");
+    }, 1000)
+})
+
+/* time */
+function displayTime() {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    const seconds = currentTime.getSeconds();
+
+    if(hours < 10) {
+        hours = "0" + hours;
+    }
+    if(minutes < 10) {
+        minutes = "0" + minutes;
+    }        
+    if(seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    const clockDiv = document.getElementById('live-clock');
+
+    clockDiv.innerText = hours + ":" + minutes + ":" + seconds;
+}
